bdf0f90dd5662cb0a72ccd27b579e6e0e4341a57
Mazhuga Sergei <ghbdtnghbdtn8@gmail.com>
2024-11-03 18:06:46 +1000

    card: fix hydration when passing `p` in description (which was also `p`)

--- components/card-description.vue
+++ components/card-description.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <p>
+  <div>
     <slot />
-  </p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-p {
+div {
   font-size: 0.9rem;
   color: #aaa;
   margin-bottom: 0.5rem;
